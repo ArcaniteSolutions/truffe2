fb63e85eada62fba2abd6ff1a6699278c6028596
Maximilien Cuony <maximilien@theglu.org>
2014-04-27 16:14:45 +0200

    Base.html and basic home

--- truffe2/app/urls.py
+++ truffe2/app/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 
 urlpatterns = patterns('',
     url(r'', include('main.urls')),
+
+    (r'^' + settings.MEDIA_URL[1:] + '(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),  # In prod, use apache !
+    (r'^' + settings.STATIC_URL[1:] + '(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),  # In prod, use apache !
 )
