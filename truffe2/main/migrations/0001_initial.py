e6be3b1c172533f35480294e04e0cf1d3f65dc33
Lionel Martin <lionel.martin@polylan.ch>
2015-05-08 21:30:55 +0200

    improve environment setup

--- truffe2/main/migrations/0001_initial.py
+++ truffe2/main/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("users", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding model 'HomePageNews'
         db.create_table(u'main_homepagenews', (
