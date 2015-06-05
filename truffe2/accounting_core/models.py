2535b0667903dd0a141139f110f799f9f1a30078
Lionel Martin <lionel.martin@polylan.ch>
2015-06-05 16:51:51 +0200

    Adding AccountingYear

---
+++ truffe2/accounting_core/models.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+from generic.models import GenericModel, GenericStateModel
+from rights.utils import AgepolyEditableModel
+
+
+class _AccountingYear(GenericModel, GenericStateModel, AgepolyEditableModel):
+
+    class MetaRightsAgepoly(AgepolyEditableModel.MetaRightsAgepoly):
+        access = 'TRESORERIE'
+
+
+    name = models.CharField(max_length=255, unique=True)
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+
+
+    class MetaState_:
+
+        unit_field = '!root'  # !root for the root unit, or the field with an unit
+
+        default = '0_preparing'
+
+        states_links = {
+            '0_preparing': ['1_active'],
+            '1_active': ['2_closing'],
+            '2_closing': ['3_archived'],
+            '3_archived': [],
+        }
+
+        states_colors = {
+            '0_preparing': 'primary',
+            '1_active': 'success',
+            '2_closing': 'warning',
+            '3_archived': 'default',
+        }
+
+        states_icons = {
+            '0_preparing': '',
+            '1_active': '',
+            '2_closing': '',
+            '3_archived': '',
+        }
+
+        list_quick_switch = {
+            '0_preparing': [('1_active', 'fa fa-check', _(u'Rendre l\'année active')), ],
+            '1_active': [('2_closing', 'fa fa-check', _(u'Passer l\'année en clôture')), ],
+            '2_closing': [('3_archived', 'fa fa-check', _(u'Archiver l\'année')), ],
+            '3_archived': [],
+        }
+
+        states_default_filter = '0_preparing,1_active,2_closing'
+        states_default_filter_related = '1_active,2_closing,3_archived'
+        status_col_id = 3
+
+    def may_switch_to(self, user, dest_state):
+
+        return self.rights_can('EDIT', user)
+
+    def can_switch_to(self, user, dest_state):
+
+        if self.status == '3_archived' and not user.is_superuser:
+            return (False, _(u'Seul un super utilisateur peut sortir cet élément de l\'état archivé'))
+
+        if int(dest_state[0]) - int(self.status[0]) != 1 and not user.is_superuser:
+            return (False, _(u'Seul un super utilisateur peut sauter des étapes ou revenir en arrière.'))
+
+        if not self.rights_can('EDIT', user):
+            return (False, _('Pas les droits.'))
+
+        return super(_AccountingYear, self).can_switch_to(user, dest_state)
