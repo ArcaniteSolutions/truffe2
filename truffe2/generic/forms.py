f2847af33d32ede5029a852da72e4fabc8ccefff
Lionel Martin <lionel.martin@polylan.ch>
2015-07-20 23:24:44 +0200

    Fix generic

--- truffe2/generic/forms.py
+++ truffe2/generic/forms.py
@@ -11,9 +11,6 @@ class GenericForm(ModelForm):
     def __init__(self, current_user, *args, **kwargs):
         super(GenericForm, self).__init__(*args, **kwargs)
 
-        if hasattr(self.instance, 'genericFormExtraInit'):
-            self.instance.genericFormExtraInit(self, current_user, *args, **kwargs)
-
         if 'user' in self.fields:
             if hasattr(self.Meta.model.MetaData, 'has_unit') and self.Meta.model.MetaData:
                 from users.models import TruffeUser
@@ -23,11 +20,13 @@ class GenericForm(ModelForm):
             from units.models import Unit
             self.fields['unit'].queryset = Unit.objects.order_by('name')
 
-
         if hasattr(self.Meta.model, 'MetaEdit') and hasattr(self.Meta.model.MetaEdit, 'only_if'):
             for key, test in self.Meta.model.MetaEdit.only_if.iteritems():
                 if not test((self.instance, current_user)):
                     del self.fields[key]
+
+        if hasattr(self.instance, 'genericFormExtraInit'):
+            self.instance.genericFormExtraInit(self, current_user, *args, **kwargs)
 
     def clean(self):
         cleaned_data = super(GenericForm, self).clean()
