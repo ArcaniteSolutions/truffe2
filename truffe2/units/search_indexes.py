23e7d694a117cc0533edd934be808c314388d76d
Maximilien Cuony <maximilien@theglu.org>
2015-08-22 16:31:39 +0200

    Remove test search files

--- truffe2/units/search_indexes.py
+++ truffe2/units/search_indexes.py
@@ -1,21 +1,21 @@
-from haystack import indexes
-from django.utils.timezone import now
-
-from units.models import Unit
-
-
-class UnitIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-    last_edit_date = indexes.DateTimeField()
-
-    def get_model(self):
-        return Unit
-
-    def index_queryset(self, using=None):
-        return self.get_model().objects.filter(deleted=False)
-
-    def prepare_last_edit_date(self, obj):
-        try:
-            return obj.last_log().when
-        except:
-            return now()
+# from haystack import indexes
+# from django.utils.timezone import now
+#
+# from units.models import Unit
+#
+#
+# class UnitIndex(indexes.SearchIndex, indexes.Indexable):
+#     text = indexes.CharField(document=True, use_template=True)
+#     last_edit_date = indexes.DateTimeField()
+#
+#     def get_model(self):
+#         return Unit
+#
+#     def index_queryset(self, using=None):
+#         return self.get_model().objects.filter(deleted=False)
+#
+#     def prepare_last_edit_date(self, obj):
+#         try:
+#             return obj.last_log().when
+#         except:
+#             return now()
