8db51611ced8e5a38cd0b573c6ee492cfc45d8f2
Maximilien Cuony <maximilien@theglu.org>
2015-08-23 21:35:32 +0200

    Tentative hotfix celery

--- truffe2/app/celery.py
+++ truffe2/app/celery.py
@@ -12,7 +12,7 @@ startup()
 
 from django.conf import settings
 
-app = Celery('truffe2', broker=settings.BROKER_URL)
+app = Celery('app', broker=settings.BROKER_URL)
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
