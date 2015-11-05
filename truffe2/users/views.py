cc2f7a875a90837ae654a7e2057849113dabc620
Maximilien Cuony <maximilien@theglu.org>
2015-11-05 10:47:13 +0100

    Hotfix: unicode

--- truffe2/users/views.py
+++ truffe2/users/views.py
@@ -259,7 +259,7 @@ def ldap_search(request):
     if retour:
         internal = filter(lambda x: not x.username_is_sciper(), internal)
 
-    retour += map(lambda x: {'id': x.username, 'text': '{} - {} {} ({}){}'.format(x.username, x.first_name, x.last_name, x.email, _(u'(Pas dans l\'annuaire EPFL !)') if x.username_is_sciper() and not retour else '')}, internal)
+    retour += map(lambda x: {'id': x.username, 'text': u'{} - {} {} ({}){}'.format(x.username, x.first_name, x.last_name, x.email, u'(Pas dans l\'annuaire EPFL !)' if x.username_is_sciper() and not retour else u'')}, internal)
 
     return HttpResponse(json.dumps(retour))
 
