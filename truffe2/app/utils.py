77f1bb6539d0ce0678800f60c4071374d4645401
Maximilien Cuony <maximilien@theglu.org>
2017-06-09 14:58:10 +0200

    EPFL-Complient emails

--- truffe2/app/utils.py
+++ truffe2/app/utils.py
@@ -117,7 +117,7 @@ def send_templated_mail(request, subject, email_from, emails_to, template, conte
     text_content = plaintext.render(d)
     html_content = htmly.render(d)
 
-    msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_FROM, emails_to, reply_to=[email_from])
+    msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_FROM, emails_to)
     msg.attach_alternative(html_content, "text/html")
     msg.send()
 
