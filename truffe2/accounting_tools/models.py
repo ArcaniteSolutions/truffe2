95bb41be1318c944ad26d56c1ddfc889b820a0e5
Téo Goddet <teo.goddet@epfl.ch>
2020-05-31 13:13:59 +0200

    Fix syntax errors

--- truffe2/accounting_tools/models.py
+++ truffe2/accounting_tools/models.py
@@ -1578,7 +1578,7 @@ class CashBookLine(ModelUsedAsLine):
         return u'{} + {}% == {}'.format(self.value, self.tva, self.value_ttc)
 
     def input_amount(self):
-       return self.value_ttc if self.is_input else 0
+        return self.value_ttc if self.is_input else 0
 
     def output_amount(self):
         return self.value_ttc if self.is_output else 0
@@ -1587,12 +1587,12 @@ class CashBookLine(ModelUsedAsLine):
         return self.input_amount() - self.output_amount()
 
     @property
-        def is_input(self):
-            return self.helper[0] in ['0', '2', '6']:
+    def is_input(self):
+        return self.helper[0] in ['0', '2', '6']
 
     @property
-        def is_output(self):
-            return self.helper[0] not in ['0', '2', '6']:
+    def is_output(self):
+        return self.helper[0] not in ['0', '2', '6']
 
     def input_amount_ht(self):
         return self.value if self.is_input else 0
