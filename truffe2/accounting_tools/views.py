5090c827679fdfe029e82a9c70ee596c48193bfe
Responsable Informatique AGEPoly <responsable.informatique@agepoly.ch>
2020-06-13 17:25:11 +0200

    Better display of incorrect state and invalid total )
    use messages instead of error
    
    use messages instead ofbrading errors

--- truffe2/accounting_tools/views.py
+++ truffe2/accounting_tools/views.py
@@ -5,8 +5,9 @@ from django.db.models import Q, Sum
 from django.http import Http404, HttpResponse
 from django.utils.text import slugify
 from django.utils.translation import ugettext_lazy as _
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.conf import settings
+from django.contrib import messages
 
 
 import datetime
@@ -162,8 +163,13 @@ def internaltransfer_csv(request, pk):
     writer = UnicodeCSVWriter(response)
     line_number = 1
     for transfer in transfers:
-        if not transfer.status[0] in ['4', '5', '6']:
-            raise Exception(_(u'Internal Transfer {0} pas à l\'état à comptabiliser').format(unicode(transfer)))
+        if not transfer.status[0] in ['2', '3']:
+            messages.warning(request, _(u'Internal Transfer {0} pas à l\'état à comptabiliser/en comptabilisation').format(unicode(transfer)))
+            if len(transfers) == 1:
+                return redirect('accounting_tools.views.internaltransfer_show', transfer.pk)
+            else:
+                return redirect('accounting_tools.views.internaltransfer_list')
+
         header_row = [u'0', line_number, transfer.transfert_date.strftime(u"%d.%m.%Y"), 100000 + transfer.pk, transfer.name, transfer.amount, transfer.amount, '', '', u'CHF', 0, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', u'INT_TF#{0}'.format(unicode(transfer.pk)), '']
         debit_row = [u'1', '', '', '', '', '', '', '', '', '', '', '', u'1', line_number, transfer.account.account_number, u'CHF', u'{0} - Débit'.format(transfer.name), transfer.amount, '', 0, '', u'Non Soumis à la TVA', u'Débit', '', transfer.transfert_date.strftime(u"%d.%m.%Y"), 0, transfer.amount, transfer.amount, 0, u'INT_TF#{0}'.format(unicode(transfer.pk)), transfer.cost_center_from.account_number]
         credit_row = [u'2', '', '', '', '', '', '', '', '', '', '', '', u'2', line_number, transfer.account.account_number, u'CHF', u'{0} - Crédit'.format(transfer.name), transfer.amount, '', 0, '', u'Non Soumis à la TVA', u'Crédit', '', transfer.transfert_date.strftime(u"%d.%m.%Y"), 0, transfer.amount, transfer.amount, 0, u'INT_TF#{0}'.format(unicode(transfer.pk)), transfer.cost_center_to.account_number]
@@ -209,7 +215,12 @@ def expenseclaim_csv(request, pk):
         if not expenseclaim.rights_can('SHOW', request.user):
             raise Http404
         if not expenseclaim.status[0] in ['4', '5', '6']:
-            raise Exception(_(u'NDF {0} pas à l\'état à comptabiliser').format(unicode(expenseclaim)))
+            messages.warning(request, _(u'NDF {0} pas à l\'état à comptabiliser/en comptabilisation').format(unicode(expenseclaim)))
+            if len(expenseclaims) == 1:
+                return redirect('accounting_tools.views.expenseclaim_show', expenseclaim.pk)
+            else:
+                return redirect('accounting_tools.views.expenseclaim_list')
+
         writer.writerow([u'0', expenseclaim_count, u'Crédit', 300000 + expenseclaim.pk, expenseclaim.logs.first().when.strftime(u"%d.%m.%Y"), expenseclaim.user.username, u'CHF', 0, u'2000', u'NDF - {0}'.format(unicode(expenseclaim)), expenseclaim.logs.first().when.strftime(u"%d.%m.%Y"), '', '', '', '', '', '', '', '', u'NDF#{0}'.format(unicode(expenseclaim.pk))])
         provider_to_export.append(expenseclaim.user)
         first = True
@@ -254,7 +265,7 @@ def expenseclaim_line_write(writer, expenseclaim, line, line_number, last_line, 
         tva_string = u'Soumis'
         tva.code = 'TOSET'
 
-    row = [u'1', '', '', '', '', '', '', '', '', '', '', expenseclaim_number * 10000 + line_number, line_number, line.value, u'NDF - {0} - {1}'.unicode(expenseclaim, line.label), line.account.account_number, tva_string, tva.code, tva.value, u'OK', u'NDF#{0}'.format(unicode(expenseclaim.pk)), expenseclaim.costcenter.account_number]
+    row = [u'1', '', '', '', '', '', '', '', '', '', '', expenseclaim_number * 10000 + line_number, line_number, line.value, u'NDF - {0} - {1}'.format(unicode(expenseclaim), line.label), line.account.account_number, tva_string, tva.code, tva.value, u'OK', u'NDF#{0}'.format(unicode(expenseclaim.pk)), expenseclaim.costcenter.account_number]
 
     if last_line:  # la dernière écriture doit être de type 2
         row[0] = u'2'
@@ -300,9 +311,18 @@ def cashbook_csv(request, pk):
         if not cashbook.rights_can('SHOW', request.user):
             raise Http404
         if not cashbook.status[0] in ['4', '5', '6']:
-            raise Exception(_(u'JDC {0} pas à l\'état à comptabiliser').format(unicode(cashbook)))
+            messages.warning(request, _(u'JDC {0} pas à l\'état à comptabiliser/en comptabilisation').format(unicode(cashbook)))
+            if len(cashbooks) == 1:
+                return redirect('accounting_tools.views.cashbook_show', cashbook.pk)
+            else:
+                return redirect('accounting_tools.views.cashbook_list')
         if not cashbook.total_incomes() == cashbook.total_outcomes():
-            raise Exception(_(u'JDC {0} pas a 0, merci de le mettre a 0').format(unicode(cashbook)))
+            messages.warning(request, _(u'JDC {0} pas a 0, merci de le mettre a 0').format(unicode(cashbook)))
+            if len(cashbooks) == 1:
+                return redirect('accounting_tools.views.cashbook_show', cashbook.pk)
+            else:
+                return redirect('accounting_tools.views.cashbook_list')
+
         writer.writerow([u'0', cashbook_count, cashbook.get_lines()[0].date.strftime(u"%d.%m.%Y"), 200000 + cashbook.pk, cashbook.name, cashbook.total_incomes(), cashbook.total_incomes(), '', '', u'CHF', 0, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', u'CASHBOOK#{0}'.format(unicode(cashbook.pk))])
         first = True
         line_count = 1
